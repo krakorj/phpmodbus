0a58acc913ab46cdcc08e9ef4eff980ae73def63
krakora.jan@gmail.com <krakora.jan@gmail.com@f8811f64-96d2-11dd-9977-d3a9e51ccf17>
2010-05-26 11:13:00 +0000

    Fix: examples corrected the actual release changes (try-catch methodology)

--- trunk/examples/example_datatype.php
+++ trunk/examples/example_datatype.php
@@ -1,18 +1,20 @@
 <?php
 
-require_once dirname(__FILE__) . '/Phpmodbus/ModbusMasterUdp.php';
+require_once dirname(__FILE__) . '/../Phpmodbus/ModbusMasterUdp.php';
 
 // Create Modbus object
-$modbus = new ModbusMasterUdp("192.168.1.1");
+$modbus = new ModbusMasterUdp("192.192.15.51");
 
-// FC 3
-// read 10 words (20 bytes) from device ID=0, address=12288
-$recData = $modbus->readMultipleRegisters(0, 12288, 10);
-
-if(!$recData) {
-  // Print error information if any
-  echo "</br>Error:</br>" . $modbus->errstr . "</br>";
-  exit;
+try {
+    // FC 3
+    // read 10 words (20 bytes) from device ID=0, address=12288
+    $recData = $modbus->readMultipleRegisters(0, 12288, 10);
+}
+catch (Exception $e) {
+    // Print error information if any
+    echo $modbus;
+    echo $e;
+    exit;
 }
 
 // Received data
@@ -27,17 +29,17 @@ $values = array_chunk($recData, 4);
 // Get float from REAL interpretation
 echo "<h3>REAL to Float</h3>\n";
 foreach($values as $bytes)
-  echo PhpType::bytes2float($bytes) . "</br>";
+    echo PhpType::bytes2float($bytes) . "</br>";
 
 // Get integer from DINT interpretation
 echo "<h3>DINT to integer </h3>\n";
 foreach($values as $bytes)
-  echo PhpType::bytes2signedInt($bytes) . "</br>";
+    echo PhpType::bytes2signedInt($bytes) . "</br>";
 
 // Get integer of float from DINT interpretation
 echo "<h3>DWORD to integer (or float) </h3>\n";
 foreach($values as $bytes)
-  echo PhpType::bytes2unsignedInt($bytes) . "</br>";
+    echo PhpType::bytes2unsignedInt($bytes) . "</br>";
 
 echo "<h2>16 bit types</h2>\n";
 // Chunk the data array to set of 4 bytes
@@ -46,12 +48,12 @@ $values = array_chunk($recData, 2);
 // Get signed integer from INT interpretation
 echo "<h3>INT to integer </h3>\n";
 foreach($values as $bytes)
-  echo PhpType::bytes2signedInt($bytes) . "</br>";
+    echo PhpType::bytes2signedInt($bytes) . "</br>";
 
 // Get unsigned integer from WORD interpretation
 echo "<h3>WORD to integer </h3>\n";
 foreach($values as $bytes)
-  echo PhpType::bytes2unsignedInt($bytes) . "</br>";
+    echo PhpType::bytes2unsignedInt($bytes) . "</br>";
 
 // Get string from STRING interpretation
 echo "<h3>STRING to string </h3>\n";
