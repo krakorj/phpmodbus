7a8b62efa367a703a6563c2e8ecd208bf2d56765
krakora.jan@gmail.com <krakora.jan@gmail.com@f8811f64-96d2-11dd-9977-d3a9e51ccf17>
2012-01-02 13:29:58 +0000

    Commentary update

--- Phpmodbus/ModbusMaster.php
+++ Phpmodbus/ModbusMaster.php
@@ -19,7 +19,7 @@ require_once dirname(__FILE__) . '/IecType.php';
 require_once dirname(__FILE__) . '/PhpType.php'; 
 
 /**
- * ModbusMasterUdp
+ * ModbusMaster
  *
  * This class deals with the MODBUS master
  *  
